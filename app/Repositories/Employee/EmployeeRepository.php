2d05a193d5caf25df45a984638072ea4316688e6
tauhedulislam0001 <tauhedulislam0001@gmail.com>
2023-03-06 12:37:39 +0600

    add and configure employee repository and employee interface

---
+++ app/Repositories/Employee/EmployeeRepository.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Repositories\Employee;
+
+use App\Repositories\Employee\EmployeeInterface as EmployeeInterface;
+use App\Models\Employee;
+
+class EmployeeRepositories implements EmployeeInterface 
+{
+    public $employee;
+    
+    public function __construct(Employee $employee)
+    {
+        $this->employee = $employee;
+    }
+
+    public function getAll()
+    {
+        return $this->employee->index();
+    }
+
+    public function createEmployee()
+    {
+        return $this->employee->create();
+    }
+
+    public function editEmployee($id)
+    {
+        return $this->employee->edit($id);
+    }
+
+    public function updateEmployee($id)
+    {
+        return $this->employee->update($id);
+    }
+
+    public function deleteEmployee($id)
+    {
+        return $this->employee->delete($id);
+    }
+
+    public function activeEmployee($id)
+    {
+        return $this->employee->activeStatus($id);
+    }
+
+    public function inactiveEmployee($id)
+    {
+        return $this->employee->inactiveStatus($id);
+    }
+
+}
