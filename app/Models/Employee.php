5e30293359f2d5f694fb286009e8b938074ea50c
tauhedulislam0001 <tauhedulislam0001@gmail.com>
2023-03-06 11:57:29 +0600

    updated employee model

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Employee extends Model
 {
     use HasFactory;
+
+    protected $table = "employees";
+
+    protected $fillable = ['full_name', 'email', 'position', 'wages', 'mobile', 'status'];
 }
